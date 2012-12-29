ea06f10388618aef1a4c4d4dd5da3fb068d6eb0e
Les Smithson <lsmithso@hare.demon.co.uk>
2012-12-29 17:23:27 +0000

    add call status signalling and el handler

---
+++ skcmd.el
@@ -0,0 +1,25 @@
+(require 'dbus)
+
+(defvar skcmd-sname "uk.co.opennet.skypecmd_service")
+(defvar skcmd-iname "uk.co.opennet.skypecmd_interface")
+(defvar skcmd-path "/SkypeObject")
+
+
+(setq skcmd-announce-enabled t)
+
+(defun skcmd-status-handler(caller_id status)
+  (when skcmd-announce-enabled
+    (dtk-tone 1200 200 t) 
+    (dtk-tone 1900 200 t)
+    (message "%s %s" caller_id status)))
+
+(defun skcmd-register-signal()
+  (dbus-register-signal
+   :session skcmd-sname skcmd-path skcmd-iname
+   "signal_call_status" 'skcmd-meta-handler ))
+
+(skcmd-register-signal)
+
+(provide 'skcmd)
+
+
