907ee78017cca66f681433efa7d87a675d0cea9d
Les Smithson <lsmithso@hare.demon.co.uk>
2013-02-08 10:04:28 +0000

    added distutils

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 from distutils.core import setup
 setup(name='skcmd',
       version='0.0.2',
-      py_modules=['skcmd']
+      py_modules=['skcmd'],
+      description='Command line interface to Skype',
+      author='Les Smithson',
+      author_email='lsmithso@hare.demon.co.uk',
+      url='http://open-networks.co.uk',
+      scripts = ['skcmd.py'],
+
       )
